0290b752c65b1886a378acc29f70313a48d7b19a
Elaine Zhu <zhiyouz@uci.edu>
2024-06-04 21:20:17 -0700

    allow user to use their own grading scale

--- main.py
+++ main.py
@@ -13,14 +13,24 @@ def print_matrix(matrix):
             elif isinstance(cell, str):
                 print(f'{cell:>4}', end=' ')
         print()
+def score_scale():
+    match_score = int(input('Enter match score: '))
+    mismatch_score = int(input('Enter mismatch score: '))
+    gap_score = int(input('Enter gap score: '))
+    return match_score, mismatch_score, gap_score
 def main():
     seq1 = input("Enter a sequence: ")
     seq2 = input("Enter another sequence: ")
 
     if valid_sequence(seq1) and valid_sequence(seq2):
         ans = input("Global Alignment or Local Alignment? Type 'G' or 'L' to generate: ")
-        if ans == 'g':
-            alignment = GlobalAlignment(seq1, seq2)
+        if ans == 'G':
+            ans = input("Do you want to change default scoring scale? Type 'Y' or 'N' to continue: ")
+            if ans == 'Y':
+                match_score, mismatch_score, gap_score = score_scale()
+                alignment = GlobalAlignment(seq1, seq2, match_score, mismatch_score,gap_score)
+            else:
+                alignment = GlobalAlignment(seq1, seq2)
             matrix = alignment.generate_matrix()
             print_matrix(matrix)
             score = alignment.score
